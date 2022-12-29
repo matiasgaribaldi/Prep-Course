fcfb11efd75993d59e1308eca9f4a0b96068da44
matiasgaribaldi <matiasgaribaldi99@gmail.com>
2022-12-28 23:47:11 -0300

    first commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,8 +4,7 @@
 const nuevaString = "Hola";
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = "2";
-
+const nuevoNum = 2;
 // Crea una variable booleana:
 const nuevoBool = null;
 
